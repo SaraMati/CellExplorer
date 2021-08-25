c149bd8ad5ca1b3323a2fda0f49cdfd697980e5e
Sara <sara.mahallati@gmail.com>
2021-08-25 10:10:06 -0400

    new preferences

--- +user_preferences/user_preferences.m
+++ +user_preferences/user_preferences.m
@@ -9,10 +9,10 @@ function preferences = user_preferences(preferences,session)
 % preferences.waveform.wfWin_sec = 0.004;         % Larger window of the waveform for filtering (to avoid edge effects). Total width in seconds [default 4ms]
 % preferences.waveform.wfWinKeep = 0.0008;        % half width of the waveform. In seconds [default 0.8ms]
 
-
+preferences.waveform.nPull = 1000;
 preferences.putativeCellType.troughToPeak_boundary = 0.56; %0.525; % Narrow interneuron assigned if troughToPeak <= 0.425ms
-preferences.waveform.wfWin_sec = 0.004;         % Larger window of the waveform for filtering (to avoid edge effects). Total width in seconds [default 4ms]
-preferences.waveform.wfWinKeep = 0.00128;        % half width of the waveform. In seconds [default 0.8ms]
+preferences.waveform.wfWin_sec = 0.005;         % Larger window of the waveform for filtering (to avoid edge effects). Total width in seconds [default 4ms]
+preferences.waveform.wfWinKeep = 0.001;        % half width of the waveform. In seconds [default 0.8ms]
 %preferences.waveform.showWaveforms = true;
 
 disp('User preferences loaded successfully')
